8a9224fd0769891480a6d31b1ee61f33ce2ab213
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-07 14:31:49 +0300

    map arrays to display to ui

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -3,10 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import statusReducer from './categories/categories';
 
-const rootReducer = combineReducers({
-  bookReducer,
-  statusReducer,
-});
+const rootReducer = combineReducers({ bookReducer, statusReducer });
 
 const store = configureStore({ reducer: rootReducer });
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -4,12 +4,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteBook } from '../Redux/books/books';
 
 function Book() {
-	const showBook = useSelector((state) => [state]);
+	const showBook = useSelector((state) => state);
 	const dispatch = useDispatch();
 	return (
 		<li className='bookList'>
 			<div className='bookContainer'>
-				<span>{showBook}</span>
+				<span>
+					{showBook.map((book) => (
+						<span>{book}</span>
+					))}
+				</span>
 				<button
 					type='button'
 					className='delete'
