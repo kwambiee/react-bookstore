4b1b0cfd694ce00a1fd24eebddff68732fedb5c0
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-07 13:18:46 +0300

    Add controlled input

--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -2,18 +2,35 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 const Addbook = () => {
-  const[Reads, setReads]=useState({title:'',author:''})
-  (
+	const [reads, setReads] = useState({ title: '', author: '' });
+	const bookAdded = (e) => {
+		setReads({
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	<div className='formContainer'>
 		<span className='addBook'>ADD NEW BOOK</span>
-		<form className='form'>
-			<input className='formTitle' type='text' placeholder='Book Title' />
-			<input className='formAuthor' type='text' placeholder='Author' />
+		<form className='form' onSubmit={bookAdded}>
+			<input
+				className='formTitle'
+				type='text'
+				name='title'
+				placeholder='Book Title'
+				value={reads.value}
+			/>
+			<input
+				className='formAuthor'
+				type='text'
+				name='author'
+				placeholder='Author'
+				value={reads.value}
+			/>
 			<button type='button' className='input-submit'>
 				Add book
 			</button>
 		</form>
 	</div>;
-)};
+};
 
 export default Addbook;
