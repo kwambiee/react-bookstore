569bb139e0f695645baa3cb5aa2f7676672cefee
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-07 18:57:44 +0300

    Fix linters

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -2,24 +2,24 @@ const BOOK_ADDED = './react-bookstore/bookreducer/BOOK_ADDED';
 const BOOK_DELETED = './react-bookstore/bookreducer/BOOK_DELETED';
 
 export const bookAdded = (book) => ({
-	type: BOOK_ADDED,
-	payload: book,
+  type: BOOK_ADDED,
+  payload: book,
 });
 
 export const deleteBook = (id) => ({
-	type: BOOK_DELETED,
-	id,
+  type: BOOK_DELETED,
+  id,
 });
 
 const initialState = [];
 
 export default function bookReducer(state = initialState, action) {
-	switch (action.type) {
-		case BOOK_ADDED:
-			return [...state, { id: state.length + 1, ...action.payload }];
-		case BOOK_DELETED:
-			return state.filter((book) => book.id !== action.id);
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case BOOK_ADDED:
+      return [...state, { id: state.length + 1, ...action.payload }];
+    case BOOK_DELETED:
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
 }
--- src/Redux/categories/categories.js
+++ src/Redux/categories/categories.js
@@ -1,15 +1,15 @@
 const STATUS_CHECKED = './react-bookstore/bookreducer/STATUS_CHECKED';
 
 export const checkStatus = () => ({
-	type: STATUS_CHECKED,
+  type: STATUS_CHECKED,
 });
 
 const initialState = 'Under Construction';
 export default function statusReducer(state = initialState, action) {
-	switch (action.type) {
-		case STATUS_CHECKED:
-			return state;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case STATUS_CHECKED:
+      return state;
+    default:
+      return state;
+  }
 }
