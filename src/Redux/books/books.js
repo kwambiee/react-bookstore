369693018ea745822fc3fe5859421fae51dd69f8
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-08 13:52:04 +0300

    Add post book to API

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -1,25 +1,50 @@
 const BOOK_ADDED = './react-bookstore/bookreducer/BOOK_ADDED';
 const BOOK_DELETED = './react-bookstore/bookreducer/BOOK_DELETED';
 
-export const bookAdded = (book) => ({
-  type: BOOK_ADDED,
-  payload: book,
-});
-
-export const deleteBook = (id) => ({
-  type: BOOK_DELETED,
-  id,
-});
+const baseUrl =
+	'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const apiKey = 'LP80sdfnevJyEHbzDpGP';
 
 const initialState = [];
 
+export const postBook = (book) => {
+	return async (dispatch) => {
+		await fetch(`${baseUrl}/apps/${apiKey}/books`, {
+			method: 'POST',
+			body: JSON.stringify(book),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		})
+			.then((response) => response.data)
+			.then((response) => {
+				dispatch({ type: BOOK_ADDED, payload: response.data });
+			});
+	};
+};
+
+// const fetchBooks = () => {
+//   return async(dispatch) =>{
+//     await fetch (`${baseUrl}/apps/${apiKey}/books`)
+//   }
+
+export const bookAdded = (book) => ({
+	type: BOOK_ADDED,
+	payload: book,
+});
+
+export const deleteBook = (id) => ({
+	type: BOOK_DELETED,
+	id,
+});
+
 export default function bookReducer(state = initialState, action) {
-  switch (action.type) {
-    case BOOK_ADDED:
-      return [...state, { id: state.length + 1, ...action.payload }];
-    case BOOK_DELETED:
-      return state.filter((book) => book.id !== action.id);
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case BOOK_ADDED:
+			return [...state, { id: state.length + 1, ...action.payload }];
+		case BOOK_DELETED:
+			return state.filter((book) => book.id !== action.id);
+		default:
+			return state;
+	}
 }
--- src/components/Addbook.js
+++ src/components/Addbook.js
@@ -1,41 +1,60 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { bookAdded } from '../Redux/books/books';
+// import { bookAdded } from '../Redux/books/books';
+import { postBook } from '../Redux/books/books';
 
 const Addbook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const dispatch = useDispatch();
+	const initialBookState = {
+		item_id: 1,
+		title: '',
+		author: '',
+		category: 'Favorites',
+	};
+	const [bookState, setBookState] = useState(initialBookState);
 
-  const handleSubmit = () => {
-    dispatch(bookAdded({ title, author }));
-    setTitle('');
-    setAuthor('');
-  };
-  return (
-    <div className="formContainer">
-      <span className="addBook">ADD NEW BOOK</span>
-      <form className="form">
-        <input
-          className="formTitle"
-          type="text"
-          name="title"
-          placeholder="Book Title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          className="formAuthor"
-          type="text"
-          name="author"
-          placeholder="Author"
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <button type="button" className="input-submit" onClick={handleSubmit}>
-          Add book
-        </button>
-      </form>
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	const handleOnChange = (event) => {
+		const { name, value } = event.target;
+		setBookState({ ...bookState, [name]: value });
+	};
+
+	const handleSubmit = () => {
+		const { title, author } = bookState;
+		dispatch(postBook({ title, author })).then((data) => {
+			setBookState({
+				item_id: data.id,
+				title: data.title,
+				author: data.author,
+				category: data.category,
+			});
+		});
+		setBookState('');
+	};
+	return (
+		<div className='formContainer'>
+			<span className='addBook'>ADD NEW BOOK</span>
+			<form className='form'>
+				<input
+					className='formTitle'
+					type='text'
+					name='title'
+					placeholder='Book Title'
+					onChange={handleOnChange}
+				/>
+				<input
+					className='formAuthor'
+					type='text'
+					name='author'
+					placeholder='Author'
+					onChange={handleOnChange}
+				/>
+				<button type='button' className='input-submit' onClick={handleSubmit}>
+					Add book
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Addbook;
