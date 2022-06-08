fdf096ec005b035a65d471667be586135002d969
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-08 07:39:43 +0300

    Add redux-thunk

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import statusReducer from './categories/categories';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
-  books: bookReducer,
-  categories: statusReducer,
+	books: bookReducer,
+	categories: statusReducer,
 });
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
 
 export default store;
