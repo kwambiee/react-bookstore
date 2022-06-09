9c0543cbca385174274afd1dd1de7db2c61d05f4
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-09 12:28:01 +0300

    Style book card

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Routes, Route, Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul className="navLinks">
-            <li>
-              <Link to="/" className="navLink">
-                Books
-              </Link>
-            </li>
-            <li>
-              <Link to="/categories" className="navLink">
-                Categories
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul className='navLinks'>
+						<li>Bookstore CMS</li>
+						<li>
+							<Link to='/' className='navLink'>
+								BOOKS
+							</Link>
+						</li>
+						<li>
+							<Link to='/categories' className='navLink'>
+								CATEGORIES
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<Routes>
+				<Route path='/' element={<Books />} />
+				<Route path='/categories' element={<Categories />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/Book.js
+++ src/components/Book.js
@@ -15,29 +15,45 @@ function Book() {
 	};
 
 	return (
-  <li className="bookList">
-    <div className="bookContainer">
-      <span>
-        {showBook.map((book) => (
-          <div key={book.id} className="eachbook">
-            {book.title}
-            {' '}
-            :
-            {book.author}
-            <button
-              type="button"
-              className="delete"
-              onClick={() => {
-									deleteHandler(book.id);
-								}}
-            >
-              Delete
-            </button>
-          </div>
+		<li className='bookList'>
+			<div className='bookContainer'>
+				<div>
+					{showBook.map((book) => (
+						<div key={book.id} className='eachbook'>
+							<div>
+								<h3 className='bookCategory'>{book.category}</h3>
+								<h1 className='bookTitle'>{book.title}</h1>
+								<h2>{book.author}</h2>
+								<div>
+									<span>Comments</span>
+									<span>
+										<button
+											type='button'
+											className='delete'
+											onClick={() => {
+												deleteHandler(book.id);
+											}}
+										>
+											Delete
+										</button>
+									</span>
+									<span>Edit</span>
+								</div>
+							</div>
+							<div>
+								<span></span>
+								<span>64%</span>
+							</div>
+							<div>
+								<h2>CURRENT CHAPTER</h2>
+								<h2>CHAPTER 17</h2>
+								<button type='button'>UPDATE PROGRESS</button>
+							</div>
+						</div>
 					))}
-      </span>
-    </div>
-  </li>
+				</div>
+			</div>
+		</li>
 	);
 }
 
