54c264dae917b29b2bffd6295b643e5a38ff94cc
kwambiee <kwambokaj2.jk@gmail.com>
2022-06-09 17:51:56 +0300

    Fix linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,9 @@
+// eslint-disable jsx-props-no-spreading
+/* eslint-disable react/prop-types */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { CircularProgressBar } from '@tomik23/react-circular-progress-bar';
+import { checkPropTypes } from 'prop-types';
 import { fetchBooks, BookDeleted } from '../Redux/books/books';
 
 const props = {
@@ -26,6 +29,9 @@ const props = {
 	number: true,
 	linearGradient: ['#ffff00', 'brown'],
 };
+props.checkPropTypes = {
+	percent: checkPropTypes.any,
+};
 
 function Book() {
 	const showBook = useSelector((state) => state.books);
@@ -40,55 +46,56 @@ function Book() {
 	};
 
 	return (
-  <li className="bookList">
-    <div className="bookContainer">
-      <div>
-        {showBook.map((book) => (
-          <div key={book.id} className="bookcard">
-            <div>
-              <h3 className="bookCategory">{book.category}</h3>
-              <h1 className="bookTitle">{book.title}</h1>
-              <h2 className="bookAuthor">{book.author}</h2>
-              <div className="actions">
-                <span className="bookActions">Comments</span>
-                <span className="rightBorder" />
-                <span>
-                  <button
-                    type="button"
-                    className="delete"
-                    onClick={() => {
+		<li className='bookList'>
+			<div className='bookContainer'>
+				<div>
+					{showBook.map((book) => (
+						<div key={book.id} className='bookcard'>
+							<div>
+								<h3 className='bookCategory'>{book.category}</h3>
+								<h1 className='bookTitle'>{book.title}</h1>
+								<h2 className='bookAuthor'>{book.author}</h2>
+								<div className='actions'>
+									<span className='bookActions'>Comments</span>
+									<span className='rightBorder' />
+									<span>
+										<button
+											type='button'
+											className='delete'
+											onClick={() => {
 												deleteHandler(book.id);
 											}}
-                  >
-  Remove
-                  </button>
-                </span>
-                <span className="rightBorder" />
-                <span className="bookActions edit">Edit</span>
-              </div>
-            </div>
-            <div>
-              <span className="progress">
-                <CircularProgressBar {...props} />
-                <span className="progReport">
-                  <p className="propPercent">{`${props.percent}%`}</p>
-                  <p className="rate">completed</p>
-                </span>
-              </span>
-            </div>
-            <span className="divider" />
-            <div className="chapter">
-              <h2>CURRENT CHAPTER</h2>
-              <h3>CHAPTER 17</h3>
-              <button type="button" className="progButton">
-                UPDATE PROGRESS
-              </button>
-            </div>
-          </div>
+										>
+											Remove
+										</button>
+									</span>
+									<span className='rightBorder' />
+									<span className='bookActions edit'>Edit</span>
+								</div>
+							</div>
+							<div>
+								<span className='progress'>
+									{/* eslint-disable-next-line */}
+									<CircularProgressBar {...props} />
+									<span className='progReport'>
+										<p className='propPercent'>{`${props.percent}%`}</p>
+										<p className='rate'>completed</p>
+									</span>
+								</span>
+							</div>
+							<span className='divider' />
+							<div className='chapter'>
+								<h2>CURRENT CHAPTER</h2>
+								<h3>CHAPTER 17</h3>
+								<button type='button' className='progButton'>
+									UPDATE PROGRESS
+								</button>
+							</div>
+						</div>
 					))}
-      </div>
-    </div>
-  </li>
+				</div>
+			</div>
+		</li>
 	);
 }
 
